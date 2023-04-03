f3796f7c020a294b92823eb792b9e9bfe1dca96b
Bao <aolei.bao@accenture.com>
2023-04-03 15:37:57 +0800

    simple 1-5

--- algo/simple/1-5.py
+++ algo/simple/1-5.py
@@ -24,7 +24,7 @@ def isValidSubsequence(array, sequence):
 
 
 def sortedSquaredArray(array):
-    array = [ number ** 2 for number in array]
+    array = [number ** 2 for number in array]
     l = 0
     r = len(array) - 1
     ans = []
@@ -36,3 +36,30 @@ def sortedSquaredArray(array):
             ans.append(array[l])
             l += 1
     return ans[::-1]
+
+
+def tournamentWinner(competitions, results):
+    score_dict = {}
+    final_winner = ""
+    max_score = -1
+    for i, competition in enumerate(competitions):
+        winner = competition[0] if results[i] == 1 else competition[1]
+        if winner not in score_dict:
+            score_dict[winner] = 3
+        else:
+            score_dict[winner] += 3
+        if score_dict[winner] > max_score:
+            max_score = score_dict[winner]
+            final_winner = winner
+    return final_winner
+
+
+def nonConstructibleChange(coins):
+    # Write your code here.
+    coins = sorted(coins)
+    res = 0
+    for i, coin in enumerate(coins):
+        if coin - res > 1:
+            return res + 1
+        res += coin
+    return res + 1
