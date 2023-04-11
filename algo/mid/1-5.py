b2fd4a46f918a9622b162b83cb22c84a576b7b1c
Bao <aolei.bao@accenture.com>
2023-04-11 09:19:59 +0800

    mid 1-3

--- algo/mid/1-5.py
+++ algo/mid/1-5.py
@@ -19,5 +19,50 @@ def threeNumberSum(array, targetSum):
     ans = []
     for idx in range(len(array) - 2):
         for r in two_sum(idx + 1, len(array) - 1, targetSum - array[idx]):
-            ans.append([array[idx], * r])
+            ans.append([array[idx], *r])
     return ans
+
+
+def smallestDifference(arrayOne, arrayTwo):
+    # Write your code here.
+    arrayOne = sorted(arrayOne)
+    arrayTwo = sorted(arrayTwo)
+    closest_diff = float("inf")
+    closest_pairs = []
+    idx1 = 0
+    idx2 = 0
+    while idx1 < len(arrayOne) and idx2 < len(arrayTwo):
+        num1 = arrayOne[idx1]
+        num2 = arrayTwo[idx2]
+        diff = abs(num1 - num2)
+        if diff < closest_diff:
+            closest_diff = diff
+            closest_pairs = [num1, num2]
+        if num1 > num2:
+            idx2 += 1
+        elif num1 < num2:
+            idx1 += 1
+        else:
+            return [num1, num2]
+
+    return closest_pairs
+
+
+def moveElementToEnd(array, toMove):
+    # Write your code here.
+    left = 0
+    right = len(array) - 1
+
+    while left < right:
+        while array[left] != toMove and left < right:
+            left += 1
+        while right > left and array[right] == toMove:
+            right -= 1
+        if left == right:
+            break
+        else:
+            array[left], array[right] = array[right], array[left]
+            left += 1
+            right -= 1
+    return array
+
